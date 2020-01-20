47cfafc29b337652658d9d9f0de62ed0ff507025
Gabriel Menezes <gabriel_oliveiramenezes@yahoo.com.br>
2020-01-20 16:25:39 -0300

    Obtendo um usuário por id

--- src/main/java/com/gabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/gabriel/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.gabriel.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.workshopmongo.domain.User;
 import com.gabriel.workshopmongo.repository.UserRepository;
+import com.gabriel.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
